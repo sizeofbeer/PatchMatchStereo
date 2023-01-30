1c9c32a1b87f0c6ea1b58e2388f04593a53e3202
sizeofbeer <1461683152@qq.com>
2023-01-30 22:45:17 +0800

    add docs.

---
+++ docs/source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PatchMatchStereo'
+copyright = '2023, sizeofbeer'
+author = 'sizeofbeer'
+release = '1.0.2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_static_path = [sphinx_rtd_theme.get_html_theme_path()]
+extensions = ['recommonmark', 'sphinx_markdown_tables']
